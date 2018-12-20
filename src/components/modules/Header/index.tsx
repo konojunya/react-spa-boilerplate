d91aade98eb2055ad006b3be866a66d538311679
konojunya <junya03dance@gmail.com>
2018-12-20 19:48:15 +0900

    init

---
+++ src/components/modules/Header/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export class Header extends React.Component {
+  public render() {
+    return (
+      <header>
+        <h1>Header</h1>
+      </header>
+    );
+  }
+}
